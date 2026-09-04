d0f46b0c67d25a2ce11bb71a30c3e63c298f2cea
mbarbine <3211492+mbarbine@users.noreply.github.com>
2026-09-04 07:34:35 +0000

    ⚡ Bolt: optimize gameLoop state history pruning and interest filtering
    - Replace Math.min(...stateHistory.keys()) spread operation with O(1) Map key iterator lookup
    - Pre-calculate INTEREST_RADIUS_SQ outside connection entity loop
    - Safeguard WebRTC data channel subscriptions against missing optional properties

--- src/transport/WebRTCTransport.ts
+++ src/transport/WebRTCTransport.ts
@@ -9,12 +9,22 @@ class WebRTCChannel implements TransportChannel {
 
   constructor(private dc: any) {
     this.id = Math.random().toString(36).substring(7);
-    this.dc.onMessage.subscribe((data: string | Buffer | Uint8Array) => {
-      for (const h of this.messageHandlers) h(data);
-    });
-    this.dc.onClose.subscribe(() => {
-      for (const h of this.closeHandlers) h();
-    });
+    if (this.dc.onMessage) {
+      this.dc.onMessage.subscribe((data: string | Buffer | Uint8Array) => {
+        for (const h of this.messageHandlers) h(data);
+      });
+    }
+    if (this.dc.onClose) {
+      this.dc.onClose.subscribe(() => {
+        for (const h of this.closeHandlers) h();
+      });
+    } else if (this.dc.stateChanged) {
+      this.dc.stateChanged.subscribe((state: string) => {
+        if (state === "closed") {
+          for (const h of this.closeHandlers) h();
+        }
+      });
+    }
   }
 
   onMessage(handler: (data: string | Buffer | Uint8Array) => void): void {
